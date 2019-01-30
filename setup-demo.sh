187129b5c8f0ba1d0bda0d74a76f3ba9ca910d9c
ladrl <lukas.laedrach@zuehlke.com>
2019-01-30 15:54:25 +0100

    Attempt to abstract over alacritty location

--- setup-demo.sh
+++ setup-demo.sh
@@ -2,15 +2,16 @@
 session_file=$1
 demo_name=${2:-Demo}
 session_name=${3:-demosession}
+base_path=${4:-`pwd`}
 
 echo Demo $demo_name with file $session_file
 echo - Kill existing sessions $session_name
 tmux kill-session -t $session_name 2> /dev/null
 echo - New detached session $session_name
-tmux new-session -s $session_name -n $demo_name -d 
+tmux new-session -s $session_name -n $demo_name -d -c $base_path bash
 
 echo - Start presentation terminal
-
+#bash -c 'open -a Alacritty.app --args -vvv --config-file $base_path/presentation.yml -e tmux attach-session -d $session_name'&
 bash -c '/Volumes/Macintosh\ HD/Applications/Alacritty.app/Contents/MacOS/alacritty --config-file ./presentation.yml -e tmux attach-session -d $session_name'&
 vim $session_file
 
